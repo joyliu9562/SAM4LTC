1f2937c957305ee31e1b0eb317cf72a058b029f0
Joy Liu <149031326+joyliu9562@users.noreply.github.com>
2025-01-22 13:47:07 +0800

    Update LLM_refine.py

--- SAM4LTC/LLM_refine.py
+++ SAM4LTC/LLM_refine.py
@@ -17,14 +17,22 @@ def ask_gpt_question(sentence, triple):
                    },
                   {"role": "user", "content":  f"""
                         Please rewrite the sentence according to the requirements.
-                        Requirements:
-
-                            \item 1. Do not change the original meaning of the sentence
-                            \item 2. Use the exactly same words in the list: [{human}, {time} {location}]
-                            \item 3. You can add, reduce, or modify some other words or phrases, such as verbs
-                            \item 4. The rewritten sentence cannot be the same as the original sentence
                         
-                        Sentence that needs to be rewritten:{sentence}
+                        Requirements:
+                        
+                         1. Do not change the original meaning of the sentence
+                         
+                         2. Use the exactly same words in [{human}, {time} {location}]
+                         
+                         3. You can add, reduce, or modify some other words or phrases, such as verbs
+                         
+                         4. The rewritten sentence cannot be the same as the original sentence
+                         
+                        Sentence that needs to be rewritten: {sentence}.
+                    
+                        You may remove some information unrelated to the given person, time and location.
+                        
+                        You only need to return me the rewritten sentence without any additional output.
                    """}]
     )
     return response.choices[0].message.content
